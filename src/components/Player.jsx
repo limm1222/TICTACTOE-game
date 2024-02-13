034fc4a56ad20a5c182de87d4a33284b074c09e2
limm1222 <limm1222@naver.com>
2024-02-13 11:39:36 +0900

    players section

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setisEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick () {
+    setisEditing((editing) => !editing);
+  }
+
+  function handleChange (event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">
+    {playerName}
+  </span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+        <input type="text" required value={playerName} onChange={handleChange}/>
+    )
+  }
+  // let btnCaption = 'Save';
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}

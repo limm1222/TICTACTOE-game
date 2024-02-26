3931d99e9c256821f706d66fce12e0d7af3b9d49
limm1222 <limm1222@naver.com>
2024-02-26 23:14:45 +0900

    틱택토 게임 완성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -11,14 +19,49 @@ function deriveActivePlayer(gameTurns) {
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
     currentPlayer = "O";
   }
-  console.log(2, currentPlayer)
+  console.log(2, currentPlayer);
 
   return currentPlayer;
 }
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    Y: "Player 2",
+  });
+  // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState("X");
+
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -29,12 +72,25 @@ function App() {
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-        ...prevTurns
+        ...prevTurns,
       ];
 
-      console.log(gameTurns)
+      console.log(gameTurns);
 
       return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
     });
   }
 
@@ -46,19 +102,21 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
-        />
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [isEditing, setisEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
-  function handleEditClick () {
+  function handleEditClick() {
     setisEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
-  function handleChange (event) {
+  function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
-  let editablePlayerName = <span className="player-name">
-    {playerName}
-  </span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   // let btnCaption = 'Edit';
 
   if (isEditing) {
     editablePlayerName = (
-        <input type="text" required value={playerName} onChange={handleChange}/>
-    )
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
   // let btnCaption = 'Save';
 
